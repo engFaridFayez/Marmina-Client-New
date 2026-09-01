e39c7a93706b241a4992a65cd300edfd6bcd4afe
Farid <faridfaiz1@outlook.com>
2026-09-01 22:45:53 +0300

    modifications

--- src/router/index.ts
+++ src/router/index.ts
@@ -353,6 +353,7 @@ const router = createRouter({
           component: () => import("@/views/admin/UpdateOwnUserPassword.vue"),
           meta: {
             requiresAuth: true,
+            requiresAdmin: true,
           }
         },
         // {
